e39dbbb9fa6ac52d23d2dba64477fa5bbf2a9e69
Rodion Kalinichev <rodik@MacBook-Air-Rodion.local>
2023-08-28 18:01:42 +0300

    Запись данных из бота в гугл таблицу. Генерирование ссылки на пдф файл и его сохранение локально

--- bot/main.go
+++ bot/main.go
@@ -2,15 +2,29 @@ package main
 
 //подключение требуемых пакетов
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
+
+	"context"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+
+	"google.golang.org/api/drive/v3"
 )
 
 // структура для приходящих сообщений и обычных кнопок
@@ -38,7 +52,28 @@ type MessageT struct {
 			Username  string `json:"username"`
 			Type      string `json:"type"`
 		} `json:"chat"`
-		Date    int `json:"date"`
+		Date     int `json:"date"`
+		Document struct {
+			FileName  string `json:"file_name"`
+			MimeType  string `json:"mime_type"`
+			Thumbnail struct {
+				FileID       string `json:"file_id"`
+				FileUniqueID string `json:"file_unique_id"`
+				FileSize     int    `json:"file_size"`
+				Width        int    `json:"width"`
+				Height       int    `json:"height"`
+			} `json:"thumbnail"`
+			Thumb struct {
+				FileID       string `json:"file_id"`
+				FileUniqueID string `json:"file_unique_id"`
+				FileSize     int    `json:"file_size"`
+				Width        int    `json:"width"`
+				Height       int    `json:"height"`
+			} `json:"thumb"`
+			FileID       string `json:"file_id"`
+			FileUniqueID string `json:"file_unique_id"`
+			FileSize     int    `json:"file_size"`
+		} `json:"document"`
 		Contact struct {
 			PhoneNumber string `json:"phone_number"`
 		} `json:"contact"`
@@ -122,7 +157,7 @@ type Product struct {
 	Type              string   `json:"type"`
 	Size              string   `json:"size"`
 	Weight            string   `json:"weight"`
-	Description       string   `json:"color"`
+	Description       string   `json:"description"`
 	Count             string   `json:"count"`
 	SelectedPackaging []string `json:"selected_packaging"`
 	Marking           string   `json:"marking"`
@@ -131,7 +166,19 @@ type Product struct {
 
 // переменные для подключения к боту
 var host string = "https://api.telegram.org/bot"
+
 var token string = os.Getenv("BOT_TOKEN")
+
+// переменная для тг канала
+var channelName string = os.Getenv("CHANEL_NAME")
+
+// Идентификатор таблицы, в которую будут записываться данные
+var spreadsheetID string = os.Getenv("SPREAD_SHEET_ID")
+
+// Замените "Sheet1" на название листа, в который вы хотите добавить данные.
+var sheetName string = os.Getenv("SHEET_NAME")
+
+var srv *drive.Service
 
 // данные всеx пользователей
 var usersDB map[int]UserT
@@ -145,6 +192,35 @@ func main() {
 
 	//обнуление последнего id сообщения
 	lastMessage := 0
+
+	ctx := context.Background()
+	b, err := os.ReadFile("client.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, _ = drive.NewService(ctx, option.WithHTTPClient(client))
+
+	r, err := srv.Files.List().PageSize(10).
+		Fields("nextPageToken, files(id, name)").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+	fmt.Println("Files:")
+	if len(r.Files) == 0 {
+		fmt.Println("No files found.")
+	} else {
+		for _, i := range r.Files {
+			fmt.Printf("%s (%s)\n", i.Name, i.Id)
+		}
+	}
 
 	//цикл для проверки на наличие новых сообщений
 	for range time.Tick(time.Second * 1) {
@@ -195,39 +271,6 @@ func getUsers() {
 	json.Unmarshal(dataFile, &usersDB)
 }
 
-// функция для отправки POST запроса
-func sendPost(requestBody string, url string) ([]byte, error) {
-	// Создаем новый POST-запрос
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString(requestBody))
-	if err != nil {
-		return nil, fmt.Errorf("Ошибка при создании запроса: %v", err)
-	}
-
-	// Устанавливаем заголовок Content-Type для указания типа данных в теле запроса
-	req.Header.Set("Content-Type", "application/json")
-
-	// Отправляем запрос с использованием стандартного клиента HTTP
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("Ошибка при выполнении запроса: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Проверяем код состояния HTTP-ответа
-	if resp.StatusCode == http.StatusOK {
-		// Успешный запрос, читаем тело ответа
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("Ошибка при чтении тела ответа: %v", err)
-		}
-		return body, nil
-	} else {
-		// Обработка ошибки при некорректном статусе HTTP-ответа
-		return nil, fmt.Errorf("Некорректный код состояния HTTP: %s", resp.Status)
-	}
-}
-
 // функция для отправки сообщения пользователю
 func sendMessage(chatId int, text string, keyboard map[string]interface{}) {
 	url := host + token + "/sendMessage?chat_id=" + strconv.Itoa(chatId) + "&text=" + text
@@ -239,6 +282,41 @@ func sendMessage(chatId int, text string, keyboard map[string]interface{}) {
 	http.Get(url)
 }
 
+// функция для отправки сообщения в канал
+func sendMessageToChanel(apiURL string) {
+	fmt.Println("sendMessage")
+	requestURL, err := url.Parse(apiURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Создание HTTP GET-запроса с параметрами
+	request, err := http.NewRequest("GET", requestURL.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Отправка запроса
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// Чтение ответа
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Вывод конечной ссылки запроса
+	finalURL := request.URL.String()
+	fmt.Println("Final URL:", finalURL)
+
+	// Вывод ответа от сервера
+	fmt.Println("Response:", string(responseData))
+}
 func processMessage(message MessageT, messageInline MessageInlineT) {
 
 	text := message.Message.Text
@@ -252,6 +330,7 @@ func processMessage(message MessageT, messageInline MessageInlineT) {
 	firstName := message.Message.From.FirstName
 	lastName := message.Message.From.LastName
 	button := messageInline.CallbackQuery.Data
+	file_id := message.Message.Document.FileID
 
 	//есть ли юзер
 	_, exist := usersDB[chatId]
@@ -512,7 +591,109 @@ func processMessage(message MessageT, messageInline MessageInlineT) {
 		user := usersDB[chatId]
 
 		productIndex := len(user.Order.Products) - 1
-		user.Order.Products[productIndex].Barcode = text
+		if user.Order.Products[productIndex].Marketplace == "yandex" || user.Order.Products[productIndex].Marketplace == "wildberris" {
+			user.Order.Products[productIndex].Barcode = text
+		} else {
+
+			// Создайте URL для получения метаданных файла.
+			metadataURL := fmt.Sprintf("https://api.telegram.org/bot%s/getFile?file_id=%s", token, file_id)
+
+			// Отправьте GET-запрос для получения метаданных файла.
+			resp, err := http.Get(metadataURL)
+			if err != nil {
+				log.Fatalf("Ошибка при запросе метаданных файла: %v", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				log.Fatalf("Ошибка: %v", resp.Status)
+			}
+
+			// Распарсите JSON-ответ, чтобы получить путь к файлу.
+			var fileInfo struct {
+				Ok     bool `json:"ok"`
+				Result struct {
+					FilePath string `json:"file_path"`
+				} `json:"result"`
+			}
+
+			if err := json.NewDecoder(resp.Body).Decode(&fileInfo); err != nil {
+				log.Fatalf("Ошибка при разборе JSON: %v", err)
+			}
+
+			// Укажите путь к папке, в которую вы хотите сохранить файл.
+			targetFolder := "barcodes" // Укажите свой путь к папке
+
+			// Проверьте, существует ли указанная папка, и создайте ее, если нет.
+			if err := os.MkdirAll(targetFolder, os.ModePerm); err != nil {
+				log.Fatalf("Ошибка при создании папки: %v", err)
+			}
+
+			// Определите имя файла на основе его пути.
+			_, fileName := filepath.Split(fileInfo.Result.FilePath)
+
+			// Создайте локальный файл в указанной папке для сохранения.
+			localFilePath := filepath.Join(targetFolder, fileName)
+			localFile, err := os.Create(localFilePath)
+			if err != nil {
+				log.Fatalf("Ошибка при создании локального файла: %v", err)
+			}
+			defer localFile.Close()
+
+			// Создайте URL для скачивания файла.
+			fileURL := fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", token, fileInfo.Result.FilePath)
+
+			// Отправьте GET-запрос для скачивания файла.
+			resp, err = http.Get(fileURL)
+			if err != nil {
+				log.Fatalf("Ошибка при скачивании файла: %v", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				log.Fatalf("Ошибка: %v", resp.Status)
+			}
+
+			// Скопируйте содержимое файла из ответа в локальный файл.
+			_, err = io.Copy(localFile, resp.Body)
+			if err != nil {
+				log.Fatalf("Ошибка при копировании содержимого: %v", err)
+			}
+
+			fmt.Printf("Файл успешно сохранен в %s\n", localFilePath)
+
+			file, err := os.Open(localFilePath)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			stat, err := file.Stat()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			defer file.Close()
+
+			res, err := srv.Files.Create(
+				&drive.File{
+					Parents: []string{"1OaWHZirxpar-eGkrSmwGDGc8CZ77N83o"},
+					Name:    "barcode.pdf",
+				},
+			).Media(file, googleapi.ChunkSize(int(stat.Size()))).Do()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Printf("%s\n", res.Id)
+
+			srv.Permissions.Create(res.Id, &drive.Permission{
+				Role: "reader",
+				Type: "anyone",
+			}).Do()
+
+			user.Order.Products[productIndex].Barcode = "https://drive.google.com/file/d/" + res.Id
+
+			fmt.Println("https://drive.google.com/file/d/" + res.Id)
+
+		}
 
 		// Собираем объект клавиатуры для выбора упаковки
 		buttons := [][]map[string]interface{}{
@@ -528,6 +709,7 @@ func processMessage(message MessageT, messageInline MessageInlineT) {
 
 		user.Step += 1
 		usersDB[chatId] = user
+
 		break
 
 	case usersDB[chatId].Step == 12 && button == "add_good":
@@ -560,7 +742,7 @@ func processMessage(message MessageT, messageInline MessageInlineT) {
 		user.Step += 1
 		usersDB[chatId] = user
 
-	case usersDB[chatId].Step == 14:
+	case usersDB[chatId].Step == 13:
 		user := usersDB[chatId]
 		user.Order.Shipment = text
 
@@ -618,6 +800,10 @@ func processMessage(message MessageT, messageInline MessageInlineT) {
 		formattedTime := newTime.Format(format)
 
 		user.Order.RegDate = formattedTime
+
+		//переменая с уникальным номером заявки
+		ticketNumber := generateUniqueTicketNumber()
+
 		// Создаем объект клавиатуры
 		keyboard := map[string]interface{}{
 			"keyboard": [][]map[string]interface{}{
@@ -638,13 +824,184 @@ func processMessage(message MessageT, messageInline MessageInlineT) {
 		jsonString, _ := json.Marshal(usersDB)
 		file.Write(jsonString)
 
+		// Загрузите файл учетных данных вашего проекта Google Cloud в переменную `credentials`.
+		credentials, err := ioutil.ReadFile("credentials.json")
+		if err != nil {
+			log.Fatalf("Unable to read client secret file: %v", err)
+		}
+
+		// Извлеките конфигурацию OAuth2 из файла учетных данных и получите токен доступа.
+		config, err := google.JWTConfigFromJSON(credentials, sheets.SpreadsheetsScope)
+		if err != nil {
+			log.Fatalf("Unable to parse client secret file to config: %v", err)
+		}
+		client := config.Client(context.Background())
+
+		// Создайте клиент Google Sheets API.
+		sheetsService, err := sheets.New(client)
+		if err != nil {
+			log.Fatalf("Unable to retrieve Sheets client: %v", err)
+		}
+
+		// Загрузите файл с данными в формате JSON.
+		content, err := ioutil.ReadFile("db.json")
+		if err != nil {
+			log.Fatalf("Unable to read data file: %v", err)
+		}
+
+		// Распарсите JSON и извлеките данные.
+		var jsonData map[string]interface{}
+		if err := json.Unmarshal(content, &jsonData); err != nil {
+			log.Fatalf("Unable to parse JSON data: %v", err)
+		}
+
+		fmt.Println(jsonData)
+
+		// Создайте список значений для новой строки.
+		var values [][]interface{}
+
+		// Проверяем существование и не nil поля "order" в jsonData
+		if orderData, ok := jsonData[strconv.Itoa(chatId)].(map[string]interface{}); ok && orderData != nil {
+
+			//текст сообщения в канал
+			chanText := "Заявка №" + ticketNumber + " от " + orderData["order"].(map[string]interface{})["reg_date"].(string) + "\n\nПринял: " + orderData["first_name"].(string) + " " + orderData["last_name"].(string) + "\n\nНаименование клиента: " +
+				orderData["order"].(map[string]interface{})["organization_name"].(string) + "\n\n"
+
+			// Проход по каждому продукту в массиве product
+			for _, product := range orderData["order"].(map[string]interface{})["product"].([]interface{}) {
+				p := product.(map[string]interface{})
+
+				// Преобразование selected_packaging в []string
+				var packaging []string
+				if selectedPackaging, ok := p["selected_packaging"].([]interface{}); ok {
+					for _, item := range selectedPackaging {
+						packaging = append(packaging, item.(string))
+					}
+				}
+
+				row := []interface{}{
+					ticketNumber,
+					orderData["first_name"],
+					orderData["last_name"],
+					orderData["order"].(map[string]interface{})["reg_date"],
+					orderData["order"].(map[string]interface{})["organization_name"],
+					p["marketplace"],
+					p["type"],
+					p["size"],
+					p["weight"],
+					p["count"],
+					p["description"],
+					p["marking"],
+					p["barcode"],
+					strings.Join(packaging, ", "),
+					orderData["order"].(map[string]interface{})["shipment"],
+					orderData["order"].(map[string]interface{})["supply"],
+					orderData["order"].(map[string]interface{})["comment"],
+				}
+
+				values = append(values, row)
+
+				chanText += "Отгрузка на " + p["marketplace"].(string) + "\n\nТовары:\n\n" + p["type"].(string) + ". Размер: " + p["size"].(string) + ". Вес: " + p["weight"].(string) + ". Количество: " + p["count"].(string) + ". Описание: " + p["description"].(string) + ". Маркировка: " +
+					p["marking"].(string) + ". Баркод: " + p["barcode"].(string) + ". Упаковка: " + strings.Join(packaging, ", ") + "\n\n"
+			}
+
+			chanText += "Отгрузка: " + orderData["order"].(map[string]interface{})["shipment"].(string) + "\n\nДата отгрузки: " + orderData["order"].(map[string]interface{})["supply"].(string) + "\n\nОсобые отметки: " + orderData["order"].(map[string]interface{})["comment"].(string)
+
+			apiURL := "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + url.QueryEscape(channelName) + "&text=" + url.QueryEscape(chanText)
+
+			sendMessageToChanel(apiURL)
+
+			// Создайте объект ValueRange для добавления новых строк.
+			rangeValue := fmt.Sprintf("%s!A2:R", sheetName)
+			vr := sheets.ValueRange{Values: values, MajorDimension: "ROWS"}
+			_, err = sheetsService.Spreadsheets.Values.Append(spreadsheetID, rangeValue, &vr).ValueInputOption("USER_ENTERED").InsertDataOption("INSERT_ROWS").Do()
+			if err != nil {
+				log.Fatalf("Unable to append values: %v", err)
+			}
+			fmt.Println("Values appended successfully.")
+		} else {
+			fmt.Println("Order data not found or is nil.")
+		}
+
+		user.Order = Order{}
+		usersDB[chatId] = user
+
+		file, _ = os.Create("db.json")
+		jsonString, _ = json.Marshal(usersDB)
+		file.Write(jsonString)
+
 		break
 
 	}
 
 }
 
-// // //сохраняем в файл для отдачи данных
-// // fileU, _ := os.Create("orders.json")
-// // data, _ := json.Marshal(user)
-// // fileU.Write(data)
+// функция для генерации случайных номеров заявок
+func generateUniqueTicketNumber() string {
+	// Получите текущее время в миллисекундах
+	currentTimeMillis := time.Now().UnixNano() / 1e6
+
+	// Сгенерируйте случайное число в заданном диапазоне (например, от 1000 до 9999)
+	rand.Seed(time.Now().UnixNano())
+	randomPart := rand.Intn(9000) + 1000
+
+	// Объедините текущее время и случайное число для создания уникального номера
+	uniqueNumber := fmt.Sprintf("%d%d", currentTimeMillis, randomPart)
+
+	return uniqueNumber
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
